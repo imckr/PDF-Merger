d1f79703c8e98717cb2e1fd1f37313463988ef97
Chandra Kumar Rajwal <122195878+imckr@users.noreply.github.com>
2024-05-16 10:44:35 +0530

    Tested on 16 May

--- server.js
+++ server.js
@@ -1,16 +1,46 @@
-import express from 'express';
-import path from 'path';
-import PDFMerger from 'pdf-merger-js';
-import multer from 'multer';
+import express from "express";
+import path from "path";
+import PDFMerger from "pdf-merger-js";
+import multer from "multer";
 
 const app = express();
 var merger = new PDFMerger();
-const upload = multer({ dest: 'uploads/' })
+const upload = multer({ dest: "uploads/" });
 app.use("/static", express.static("public"));
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const port = 3000;
+
+// function dragOverHandler(ev) {
+//   console.log("File(s) in drop zone");
+
+//   // Prevent default behavior (Prevent file from being opened)
+//   ev.preventDefault();
+// }
+// function dropHandler(ev) {
+//   console.log("File(s) dropped");
+
+//   // Prevent default behavior (Prevent file from being opened)
+//   ev.preventDefault();
+
+//   if (ev.dataTransfer.items) {
+//     // Use DataTransferItemList interface to access the file(s)
+//     [...ev.dataTransfer.items].forEach((item, i) => {
+//       // If dropped items aren't files, reject them
+//       if (item.kind === "file") {
+//         const file = item.getAsFile();
+//         console.log(`… file[${i}].name = ${file.name}`);
+//       }
+//     });
+//   } else {
+//     // Use DataTransfer interface to access the file(s)
+//     [...ev.dataTransfer.files].forEach((file, i) => {
+//       console.log(`… file[${i}].name = ${file.name}`);
+//     });
+//   }
+// }
+
 
 const mergerPDFs = async (p1, p2) => {
   await merger.add(p1); //merge all pages. parameter is the path to file and filename.
@@ -34,20 +64,22 @@ const mergerPDFs = async (p1, p2) => {
   // Export the merged PDF as a nodejs Buffer
   // const mergedPdfBuffer = await merger.saveAsBuffer();
   // fs.writeSync('merged.pdf', mergedPdfBuffer);
-}
+};
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "template/index.html"));
 });
 
 app.post("/merge", upload.array("pdfs", 2), function (req, res, next) {
-    console.log(req.files)
-    mergerPDFs(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path))
-    res.redirect("http://localhost:3000/static/merged.pdf")
-    // req.files is array of `pdf` files
-    // req.body will contain the text fields, if there were any
+  console.log(req.files);
+  mergerPDFs(
+    path.join(__dirname, req.files[0].path),
+    path.join(__dirname, req.files[1].path)
+  );
+  res.redirect("http://localhost:3000/static/merged.pdf");
+  // req.files is array of `pdf` files
+  // req.body will contain the text fields, if there were any
 });
-
 
 app.listen(port, () => {
   console.log(`app listening on port http://localhost:${port}`);
